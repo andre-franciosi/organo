90ffc963d296eaf09cf4ec1bd3da42ad21e9d6ed
André Franciosi <andref2210@hotmail.com>
2024-02-29 16:46:06 -0300

    Add ListaSuspensa
    Adicionando lista suspensa de países
    Definição de que o site vai ser uma lista de times da Copa do Mundo de 2022

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,14 +1,25 @@
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
 import "./Formulario.css"
 
 function Formulario() {
+
+    const paises = [
+        'Argentina',
+        'Brasil',
+        'Bélgica',
+        'Alemanha',
+        'Inglaterra'
+    ]
+
     return (
         <section className="formulario">
             <form>
                 <h2> Preencha os dados para criar um card</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto label="Posição" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label = "País" itens={paises}/>
             </form>
         </section>
     )
